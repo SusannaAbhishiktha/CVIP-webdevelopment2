b8aadd931f87d3fbf60521550d14b2b896cdf18e
SusannaAbhishiktha <141997454+SusannaAbhishiktha@users.noreply.github.com>
2023-12-26 19:53:52 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navToggle = document.getElementById('nav-toggle');
+    const navMenu = document.getElementById('nav-menu');
+
+    if (navToggle && navMenu) {
+        navToggle.addEventListener('click', function () {
+            navMenu.classList.toggle('show');
+        });
+    }
+});
